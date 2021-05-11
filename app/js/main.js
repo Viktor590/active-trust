9ab9693f2eead76331318de0aac362878dcce82e
Viktor590 <resident1081994@gmail.com>
2021-05-11 18:17:50 +0300

    added hover in button slider

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,8 @@ $(function () {
     dots: false,
     arrow: true,
     infinite: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left-slider.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right-slider.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg width="19" height="16" viewBox="0 0 19 16"><path d="M0.292892 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292892 7.29289ZM19 7L1 7V9L19 9V7Z"  fill="#027BFD" fill-opacity="0.5"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg width="20" height="16" viewBox="0 0 20 16"><path d="M19.7071 8.70711C20.0976 8.31658 20.0976 7.68342 19.7071 7.29289L13.3431 0.928933C12.9526 0.538409 12.3195 0.538409 11.9289 0.928933C11.5384 1.31946 11.5384 1.95262 11.9289 2.34315L17.5858 8L11.9289 13.6569C11.5384 14.0474 11.5384 14.6805 11.9289 15.0711C12.3195 15.4616 12.9526 15.4616 13.3431 15.0711L19.7071 8.70711ZM-8.74227e-08 9L19 9L19 7L8.74227e-08 7L-8.74227e-08 9Z" fill="#027BFD" fill-opacity="0.5"/></svg></button>',
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -39,9 +39,6 @@ $(function () {
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
 });
