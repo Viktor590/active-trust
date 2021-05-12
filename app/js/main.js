e893a15b44f0a9ebdaf34483c8f5483806d2ed96
Viktor590 <resident1081994@gmail.com>
2021-05-12 14:51:44 +0300

    added adaptive for header

--- app/js/main.js
+++ app/js/main.js
@@ -41,4 +41,9 @@ $(function () {
       }
     ]
   });
+
+  $('.header__burger').on('click' ,function() {
+    $('.header__burger, .header__burger-span, .header__nav').toggleClass('active')
+    $('body').toggleClass('lock')
+  });
 });
